466cfdf9c13506c3edf0d82d7f73ebf44c05ca8e
sho0o8 <monchan0819@gmail.com>
2021-07-15 09:10:58 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Referral
                 </div>
 
                 <div class="links">
